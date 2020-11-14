c88c298748a17fe7743765adbd64c4d9faffda50
violateer <1828257089@qq.com>
2020-11-14 17:20:01 +0800

    显示时间2

--- src/index.js
+++ src/index.js
@@ -11,17 +11,34 @@ ReactDOM.render(
 );
 
 // 实现时刻的显示
-function clock() {
-  const time = new Date().toLocaleTimeString();
-  const element = (
+// function clock() {
+//   const time = new Date().toLocaleTimeString();
+//   const element = (
+//     <div>
+//       <h1>现在的时间是：{time}</h1>
+//     </div>
+//   );
+//   const root = document.querySelector('#root');
+//   ReactDOM.render(element, root);
+// }
+
+// clock();
+
+// setInterval(clock, 1000);
+
+// react函数式组件
+function Clock(props) {
+  return (
     <div>
-      <h1>现在的时间是：{time}</h1>
+      <h1>现在的时间是：{props.date.toLocaleTimeString()}</h1>
     </div>
   );
-  const root = document.querySelector('#root');
-  ReactDOM.render(element, root);
 }
 
-clock();
+function run() {
+  ReactDOM.render(<Clock date={new Date()} />, document.querySelector('#root'));
+}
 
-setInterval(clock(), 1000);
+run();
+
+setInterval(run, 1000);
