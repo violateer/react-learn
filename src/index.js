b3c42edaeca68a92d7183fbfffd61031c68ff269
violateer <1828257089@qq.com>
2020-11-14 17:08:47 +0800

    显示时间1

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -11,7 +10,18 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// 实现时刻的显示
+function clock() {
+  const time = new Date().toLocaleTimeString();
+  const element = (
+    <div>
+      <h1>现在的时间是：{time}</h1>
+    </div>
+  );
+  const root = document.querySelector('#root');
+  ReactDOM.render(element, root);
+}
+
+clock();
+
+setInterval(clock(), 1000);
