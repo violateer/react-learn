ce98d3b36e3d0c4214eaff34beb1bdba9f313258
violateer <1828257089@qq.com>
2020-11-15 16:41:39 +0800

    显示时间2

--- src/index.js
+++ src/index.js
@@ -12,17 +12,29 @@ class Clock extends React.Component {
   }
 
   render() {
-    this.state.time = new Date().toLocaleTimeString();
+    // this.state.time = new Date().toLocaleTimeString();
     return (
       <div>
         <h1>当前时间：{this.state.time}</h1>
       </div>
     );
   }
+
+  // 生命周期函数 componentDidMount  组件渲染完成时调用
+  componentDidMount() {
+    setInterval(() => {
+      // 不要用这种方式
+      // this.state.time = new Date().toLocaleTimeString();
+
+      this.setState({
+        time: new Date().toLocaleTimeString(),
+      });
+    }, 1000);
+  }
 }
 
 ReactDOM.render(<Clock />, document.querySelector('#root'));
 
-setInterval(() => {
-  ReactDOM.render(<Clock />, document.querySelector('#root'));
-}, 1000);
+// setInterval(() => {
+//   ReactDOM.render(<Clock />, document.querySelector('#root'));
+// }, 1000);
