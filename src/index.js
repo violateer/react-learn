472fd63f09803c4cd8a5fcd0927b79dcd36acaad
violateer <1828257089@qq.com>
2020-11-15 16:09:19 +0800

    函数式组件和类组件

--- src/index.js
+++ src/index.js
@@ -1,19 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './App.css';
 
-// const exampleStyle = {
-//   background: 'skyblue',
-//   borderBottom: '1px solid red',
-// };
+// 函数式组件
+// function ChildCom() {
+//   const subTitle = <span>函数式组件副标题</span>;
+//   const weather = 'good';
+//   const isGo = weather === 'good' ? '出门' : '不出门';
+//   return (
+//     <div>
+//       <h1>函数式组件Hello World</h1>
+//       {subTitle}
+//       <p>{isGo}</p>
+//     </div>
+//   );
+// }
 
-const exampleStyle = ['font', 'bg'].join(' ');
+// 类组件
+class ChildCom extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>类组件Hello World</h1>
+      </div>
+    );
+  }
+}
 
-const element = (
-  <div>
-    {/* 这里写注释 */}
-    <h1 className={exampleStyle}>Hello World</h1>
-  </div>
-);
-
-ReactDOM.render(element, document.querySelector('#root'));
+ReactDOM.render(<ChildCom />, document.querySelector('#root'));
